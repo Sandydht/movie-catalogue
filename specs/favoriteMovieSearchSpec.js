8fab96997b4a11fcbbd0466511b12525938604ef
Sandydht <sandydwi75@gmail.com>
2023-06-10 11:16:11 +0700

    feat: finish search movie

--- specs/favoriteMovieSearchSpec.js
+++ specs/favoriteMovieSearchSpec.js
@@ -43,9 +43,9 @@ describe('Searching movies', () => {
     });
 
     it('should show the movies found by Favorite Movies', (done) => {
-      document.getElementById('movie-search-container')
-        .addEventListener('movies:searched:updated', () => {
-          expect(document.querySelectorAll('.movie').length).toEqual(3);
+      document.getElementById('movies')
+        .addEventListener('movies:updated', () => {
+          expect(document.querySelectorAll('.movie-item').length).toEqual(3);
           done();
         });
 
@@ -59,8 +59,8 @@ describe('Searching movies', () => {
     });
 
     it('should show the name of the movies found by Favorite Movies', (done) => {
-      document.getElementById('movie-search-container')
-        .addEventListener('movies:searched:updated', () => {
+      document.getElementById('movies')
+        .addEventListener('movies:updated', () => {
           const movieTitles = document.querySelectorAll('.movie__title');
           expect(movieTitles.item(0).textContent).toEqual('film abc');
           expect(movieTitles.item(1).textContent).toEqual('ada juga film abcde');
@@ -78,7 +78,7 @@ describe('Searching movies', () => {
     });
 
     it('should show - when the movie returned does not contain a title', (done) => {
-      document.getElementById('movie-search-container').addEventListener('movies:searched:updated', () => {
+      document.getElementById('movies').addEventListener('movies:updated', () => {
         const movieTitles = document.querySelectorAll('.movie__title');
         expect(movieTitles.item(0).textContent).toEqual('-');
         done();
@@ -115,9 +115,9 @@ describe('Searching movies', () => {
 
   describe('When no favorite movies could be found', () => {
     it('should show the empty message', (done) => {
-      document.getElementById('movie-search-container')
-        .addEventListener('movies:searched:updated', () => {
-          expect(document.querySelectorAll('.movies__not__found').length).toEqual(1);
+      document.getElementById('movies')
+        .addEventListener('movies:updated', () => {
+          expect(document.querySelectorAll('.movie-item__not__found').length).toEqual(1);
           done();
         });
 
@@ -126,8 +126,8 @@ describe('Searching movies', () => {
     });
 
     it('should not show any movie', (done) => {
-      document.getElementById('movie-search-container').addEventListener('movies:searched:updated', () => {
-        expect(document.querySelectorAll('.movie').length).toEqual(0);
+      document.getElementById('movies').addEventListener('movies:updated', () => {
+        expect(document.querySelectorAll('.movie-item').length).toEqual(0);
         done();
       });
 
